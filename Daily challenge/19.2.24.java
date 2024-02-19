e087e2ac7dc3982915c6542da9b0385556591b97
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2024-02-19 18:25:53 +0530

    Add files via upload

---
+++ Daily challenge/19.2.24.java
@@ -0,0 +1,15 @@
+A table called book is created with the following DDL command:
+create table book(id int, name varchar(20), author varchar(20), publishedyear int);
+Write the Java code using JDBC APIs to select the name and author for each book whose publishedyear is NOT equal to 2009.
+Note:
+The order of the columns in the result set must be name and author.
+The result set must be ordered by the column name in ascending order.
+IMPORTANT:
+1. Place your code inside the execute method. You need not code/implement the main method.
+2. DO NOT close the Database Connection conn in your code.
+
+
+---------------------solution--------------------
+
+
+**coming soon**
