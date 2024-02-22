7d351dd1ddfadf5cf2b6d181d798e2433d9096c4
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2024-02-22 15:31:51 +0530

    Add files via upload

---
+++ Daily challenge/22.2.24.py
@@ -0,0 +1,22 @@
+Given a string S as the input, the program must print the mismatch alphabet (which is not in sequence) in the string.
+Boundary Condition(s):
+4 <= Length of S <= 10
+Input Format:
+The first line contains the string S.
+Output Format:
+The first line contains the mismatch alphabet in the string.
+Example Input/Output 1:
+Input:
+aergc
+Output:
+r
+Explanation:
+Arrange the alphabets of the string in a series. Then find the mismatch alphabet so that the string has the series of alphabets (aceg).
+Example Input/Output 2:
+Input:
+rpmsqut
+Output:
+m
+---------------solution---------------------
+
+ 
