5622df93e869c0878119d9803774b1922680b715
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2024-02-25 10:54:33 +0530

    25.2.24.py

---
+++ Daily Test/25.2.24.py
@@ -0,0 +1,23 @@
+The program must accept two string values S1 and S2 containing only alphabets as the input. 
+The program must print yes if all the upper case alphabets (A to Z) present only once in two string 
+values. Else the program must print no as the output. 
+Boundary Condition(s): 1 <= Length of S1, S2 <= 100 
+Input Format: The first line contains S1. The second line contains S2.
+Output Format: The first line contains yes or no. 
+Example Input/Output 1:
+Input:
+ABCDEFGHIJKLMNO 
+PQRSTUVWXYZ 
+Output: yes
+Explanation: Here all the upper case alphabets (A to Z) occur only once in two string values.
+So yes is printed.
+Example Input/Output 2:
+Input: FEDCBA 
+GHIJKLMNOPQRSTUVWXYZ 
+Output: yes 
+Example Input/Output 3:
+Input: CBADEFGHIjkl
+MNOPQRSTuvwXYZ 
+Output: no
+-------------------------------------solution-------------------
+
