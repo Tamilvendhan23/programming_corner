5f30c7524891b97c30498a647378a276f24116cb
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2024-03-02 15:42:09 +0530

    2.3.24.py

---
+++ Daily challenge/2.3.24.py
@@ -0,0 +1,48 @@
+"The program must accept a string S as the input. The program must print the desired pattern as shown in the Example Input/Output section.
+
+Boundary Condition(s):
+4 <= Length of S <= 50
+
+Input Format:
+The first line contains S.
+
+Output Format:
+The lines containing the desired pattern as shown in the Example Input/Output section.
+ 
+Example Input/Output 1:
+Input:
+skill
+
+Output:
+ssskillllll*
+sskkiillll**
+skkkiiill***
+
+Example Input/Output 2:
+Input:
+monday
+
+Output:
+mmmmonddddaaaay
+mmmoonndddaaayy
+mmooonnnddaayyy
+moooonnnndayyyy
+
+Example Input/Output 3:
+Input:
+FLUORESCENT
+
+Output:
+FFFFFFFFFLUOOOOOOOOORRRRRRRRRESCCCCCCCCCEEEEEEEEENT*********
+FFFFFFFFLLUUOOOOOOOORRRRRRRREESSCCCCCCCCEEEEEEEENNTT********
+FFFFFFFLLLUUUOOOOOOORRRRRRREEESSSCCCCCCCEEEEEEENNNTTT*******
+FFFFFFLLLLUUUUOOOOOORRRRRREEEESSSSCCCCCCEEEEEENNNNTTTT******
+FFFFFLLLLLUUUUUOOOOORRRRREEEEESSSSSCCCCCEEEEENNNNNTTTTT*****
+FFFFLLLLLLUUUUUUOOOORRRREEEEEESSSSSSCCCCEEEENNNNNNTTTTTT****
+FFFLLLLLLLUUUUUUUOOORRREEEEEEESSSSSSSCCCEEENNNNNNNTTTTTTT***
+FFLLLLLLLLUUUUUUUUOORREEEEEEEESSSSSSSSCCEENNNNNNNNTTTTTTTT**
+FLLLLLLLLLUUUUUUUUUOREEEEEEEEESSSSSSSSSCENNNNNNNNNTTTTTTTTT*
+
+  ------------------------------------solution---------------------------------------
+
+  
