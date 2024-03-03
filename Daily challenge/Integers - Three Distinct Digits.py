18ad125ed235f8c0074486c4e6557653585f2d6e
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2024-03-03 12:28:02 +0530

    3.3.24

---
+++ Daily challenge/Integers - Three Distinct Digits.py
@@ -0,0 +1,41 @@
+
+
+The program accepts N integers and prints the integers which are having at least three distinct digits. If there is no such integer, the program must print -1 as the output.
+
+Boundary Condition(s):
+1 <= N <= 100
+100 <= Each integer value <= 10^8
+
+Input Format:
+The first line contains N.
+The second line contains N integers separated by a space.
+
+Output Format:
+The first line contains the integers which are having at least three distinct digits separated by a space or -1.
+
+Example Input/Output 1:
+Input:
+5
+488 978 1432 151 5070
+
+Output:
+978 1432 5070
+
+Explanation:
+The integer 488 contains 2 distinct digits.
+The integer 978 contains 3 distinct digits.
+The integer 1432 contains 4 distinct digits.
+The integer 151 contains 2 distinct digits.
+The integer 5070 contains 3 distinct digits.
+Hence the output is 978 1432 5070
+
+Example Input/Output 2:
+Input:
+4
+100 474 255 666
+
+Output:
+-1
+
+---------------------------solution------------------------
+
