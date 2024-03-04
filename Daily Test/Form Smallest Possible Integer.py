cd5d75ac14689e3583e4a171056f0846126cb40e
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2024-03-04 11:55:48 +0530

    4.3.24

---
+++ Daily Test/Form Smallest Possible Integer.py
@@ -0,0 +1,30 @@
+The program must accept a string S containing only digits as the input. The program must form the smallest possible integer X using the digits in S. Finally, the program must print the value of X as the output.
+
+Boundary Condition(s):
+1 <= Length of S <= 1000
+
+Input Format:
+The first line contains S.
+
+Output Format:
+The first line contains X.
+
+Example Input/Output 1:
+Input:
+5413045415601160561561060606606067888799932266544445524554841887021
+
+Output:
+1000000000111111122223344444444445555555555666666666666777888888999
+
+Explanation:
+The smallest possible integer formed using the digits in 5413045415601160561561060606606067888799932266544445524554841887021 is 1000000000111111122223344444444445555555555666666666666777888888999.
+Hence the output is 1000000000111111122223344444444445555555555666666666666777888888999
+
+Example Input/Output 2:
+Input:
+19830000189165435362390019123121
+
+Output:
+10000001111112223333345566889999
+
+ ---------------------solution---------------------
