f5bd63168807885a277443e2ff51fec1fdd37517
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2024-03-06 13:22:42 +0530

    Create Days in Month.py

---
+++ Daily challenge/Days in Month.py
@@ -0,0 +1,35 @@
+The program must accept an integer M as the input. The program must print the number of days in the month M if it is valid. Else the program must print Invalid as the output.
+Note: The number of days in each month will be considered from a non-leap year.
+
+Boundary Condition(s):
+1 <= M <= 50
+
+Input Format:
+The first line contains M.
+
+Output Format:
+The first line contains the number of days in the month M or Invalid.
+
+Example Input/Output 1:
+Input:
+5
+
+Output:
+31
+
+Explanation:
+In a non-leap year, there are 31 days in the 5th month.
+So 31 is printed.
+
+Example Input/Output 2:
+Input:
+2
+
+Output:
+28
+
+Example Input/Output 3:
+Input:
+15
+-----------------------Solution------------------
+
