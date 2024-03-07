94e4bfc1769005d602df6c21ff48ccf053783cf2
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2024-03-07 15:31:56 +0530

    6.3.24

--- Daily challenge/Days in Month.c
+++ Daily challenge/Days in Month.c
@@ -32,4 +32,29 @@ Example Input/Output 3:
 Input:
 15
 -----------------------Solution------------------
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int num;scanf("%d",&num);
+    switch(num)
+    {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+        printf("31");break;
+        case 2:
+        printf("28");break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:printf("30");break;
+        default:printf("Invalid");
+    }
+}
 
