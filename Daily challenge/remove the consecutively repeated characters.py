e7cb078bb414eeb0870456ccc2a569709e09383f
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2024-03-08 14:48:42 +0530

    7.3.24

--- Daily challenge/remove the consecutively repeated characters.py
+++ Daily challenge/remove the consecutively repeated characters.py
@@ -22,3 +22,14 @@ Output:
 #5#C5C#5
 -------------------------solution---------------
 
+Your code below
+a=input()
+if a[0]!=a[1]:
+    print(a[0],end="")
+for i in range(1,len(a)-1):
+    if a[i]==a[i-1] or a[i]==a[i+1]:
+        continue
+    else:
+        print(a[i],end="")
+if a[-1]!=a[-2]:
+    print(a[-1])
