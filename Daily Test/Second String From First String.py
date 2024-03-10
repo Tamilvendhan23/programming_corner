7789780b395710f9a1633001124abbe3335226d7
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2024-03-10 15:54:58 +0530

    Create Second String From First String.py

---
+++ Daily Test/Second String From First String.py
@@ -0,0 +1,25 @@
+
+The program must accept two string values S1 and S2 are of equal length as the input. The program must print yes if the second string is formed using all the alphabets in the first string by ignoring the case of alphabets. Else the program must print no as the output.
+Boundary Condition(s):
+1 <= Length of S1, S2 <= 1000
+Input Format:
+The first line contains S1.
+The second line contains S2.
+Output Format:
+The first line contains yes or no.
+Example Input/Output 1:
+Input:
+Flow
+Wolf
+Output:
+yes
+Explanation:
+Here the string Wolf is formed using all the alphabets in the string Flow by ignoring the case of alphabets.
+So yes is printed as the output.
+Example Input/Output 2:
+Input:
+clOud
+robOt
+Output:
+no
+--------------------------solution--------------------------
