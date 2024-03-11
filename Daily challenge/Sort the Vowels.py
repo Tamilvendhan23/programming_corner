b714ae905a43da0ad7c98d70862476048fb17998
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2024-03-11 10:22:10 +0530

    Create Sort the Vowels.py

---
+++ Daily challenge/Sort the Vowels.py
@@ -0,0 +1,30 @@
+
+
+The program must accept a string S as the input. The program must arrange the vowels in the string S in sorted order. Finally, the program must print the modified string S as the output.
+
+Boundary Condition(s):
+3 <= Length of S <= 100
+
+Input Format:
+The first line contains S.
+
+Output Format:
+The first line contains the modified string S.
+
+Example Input/Output 1:
+Input:
+skillrack
+
+Output:
+skallrick
+
+Explanation:
+After arranging the vowels in the string skillrack in sorted order, the string becomes skallrick.
+Hence the output is skallrick
+
+Example Input/Output 2:
+Input:
+PROGRAMMING
+
+Output:
+PRAGRIMMONG
