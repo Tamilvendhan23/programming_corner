4f41dcc7476efa96da5c23cadffcacf830527090
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2024-03-21 18:19:39 +0530

    Create Two Triangles - Asterisks Pattern.py

---
+++ Daily challenge/Two Triangles - Asterisks Pattern.py
@@ -0,0 +1,30 @@
+The program must accept an integer N as the input. The program must print the desired pattern as shown in the Example Input/Output section.
+Note: The value of N is always odd.
+Boundary Condition(s):
+3 <= N <= 99
+Input Format:
+The first line contains N.
+Output Format:
+The lines containing the desired pattern as shown in the Example Input/Output section.
+Example Input/Output 1:
+Input:
+5
+Output:
+*   *
+** **
+*****
+** **
+*   *
+Example Input/Output 2:
+Input:
+7
+Output:
+*     *
+**   **
+*** ***
+*******
+*** ***
+**   **
+*     *
+  -------------------------------------------solution---------------------------------------------
+  
