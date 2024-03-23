87028aedc5a80db8100af2a98523b839a3ad3133
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2024-03-23 16:44:25 +0530

    Create Sort Same - Starts and Ends.py

---
+++ Daily Test/Sort Same - Starts and Ends.py
@@ -0,0 +1,49 @@
+The program must accept N string values containing only lower case alphabets as the input. The program must sort the string values starting and ending with the same alphabet. Then the program must print the N modified string values as the output.
+Boundary Condition(s):
+2 <= N <= 50
+2 <= Length of each string value <= 100
+Input Format:
+The first line contains N.
+The next N lines, each containing a string.
+Output Format:
+The first N lines, each containing a string representing the N modified string values.
+Example Input/Output 1:
+Input:
+5
+rotator
+madam
+auto
+bomb
+travels
+Output:
+bomb
+madam
+auto
+rotator
+travels
+Explanation:
+The string values starting and ending with the same alphabet are rotator, madam and bomb.
+After sorting those 3 string values in their positions, the string values become
+bomb
+madam
+auto
+rotator
+travels
+Example Input/Output 2:
+Input:
+6
+river
+steps
+chair
+libel
+window
+heath
+Output:
+heath
+libel
+chair
+river
+steps
+window
+----------------------------solution----------------------------------------
+  
