5b68c85c23006cb60b4fa0867f8a4f7b047f4d1b
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2024-03-24 18:13:12 +0530

    23.3.24

--- Daily challenge/Two Halves - Equal or Not.py
+++ Daily challenge/Two Halves - Equal or Not.py
@@ -26,3 +26,11 @@ LioeLin
 Output:
 NO
 -----------------------------solution-----------------------------
+def f(a):
+ l=len(a)
+ for i in range(l):
+  b=a[:i]+a[i+1:]
+  if b[:l//2]==b[l//2:]:
+      return "YES"
+ return "NO"
+print(f(input().strip()))
