cf680c34a85328efbb97ea76831017b41a8ad273
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2024-03-26 21:59:57 +0530

    Create N Integers - Kth Largest Integer.py

---
+++ Daily challenge/N Integers - Kth Largest Integer.py
@@ -0,0 +1,29 @@
+The program must accept N integers and an integer K as the input. The program must print the Kth largest integer among the N integers as the output.
+Boundary Condition(s):
+1 <= N, K <= 1000
+1 <= Each integer value  <= 10^8
+Input Format:
+The first line contains N and K separated by a space.
+The second line contains N integers separated by a space.
+Output Format:
+The first line contains the Kth largest integer among the N integers.
+Example Input/Output 1:
+Input:
+9 3
+38 92 83 58 78 86 20 92 81
+Output:
+83
+Explanation:
+Here N=9 and K=3,
+The 1st largest integer is 92.
+The 2nd largest integer is 86.
+The 3rd largest integer is 83, so 83 is printed as the output.
+Example Input/Output 2:
+Input:
+6 4
+87 94 91 79 87 87
+Output:
+79
+----------------solution------------------------
+
+
