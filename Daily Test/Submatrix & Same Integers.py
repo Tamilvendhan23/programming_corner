ba9940a15b4bb21575bfa7a4df35d74110837790
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2024-03-27 22:32:30 +0530

    Create Submatrix & Same Integers.py

---
+++ Daily Test/Submatrix & Same Integers.py
@@ -0,0 +1,38 @@
+The program must accept an integer matrix of size NxN and an integer K as the input. The program must print yes if each KxK submatrix of the given matrix contains the same integer. Else the program must print no as the output.
+Note: The integer N is always divisible by the integer K.
+Boundary Condition(s):
+2 <= K <= N <= 50
+Input Format:
+The first line contains N.
+The next N lines, each containing N integers separated by a space.
+The next (N+2)nd line contains K.
+Output Format:
+The first line contains yes or no.
+Example Input/Output 1:
+Input:
+9
+1 1 1 2 2 2 4 4 4
+1 1 1 2 2 2 4 4 4
+1 1 1 2 2 2 4 4 4
+5 5 5 3 3 3 7 7 7
+5 5 5 3 3 3 7 7 7
+5 5 5 3 3 3 7 7 7
+9 9 9 1 1 1 5 5 5
+9 9 9 1 1 1 5 5 5
+9 9 9 1 1 1 5 5 5
+3
+Output:
+yes
+Explanation:
+In the 9x9 matrix, each 3x3 submatrix contains the same integer.
+Hence the output is yes
+Example Input/Output 2:
+Input:
+4
+10 10 20 20
+10 10 20 20
+33 33 40 44
+33 33 40 44
+2
+Output:
+no
