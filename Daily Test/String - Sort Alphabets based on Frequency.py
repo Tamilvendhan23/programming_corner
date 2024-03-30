e1f71aeec1936e644ff6e14d7194dd3a48700c4b
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2024-03-30 18:31:41 +0530

    Create String - Sort Alphabets based on Frequency.py

---
+++ Daily Test/String - Sort Alphabets based on Frequency.py
@@ -0,0 +1,26 @@
+ 
+The program must accept a string S containing only alphabets as the input.
+The program must sort the alphabets in the string S based on the frequency 
+(i.e., the number of occurrences) of the alphabets in descending order. If more than one alphabets have the same frequency,
+the program must sort those alphabets in alphabetical order. Finally, the program must print the modified string as the output. 
+Boundary Condition(s):
+1 <= Length of S <= 1000  
+Input Format:
+The first line contains S.
+Output Format:
+The first line contains the modified string S. 
+Example Input/Output 1:
+Input: 
+aaajjbbbcccc 
+Output: 
+ccccaaabbbjj 
+Explanation: Here the string is aaajjbbbcccc. The frequency of a is 3. The frequency of j is 2. The frequency of b is 3. The frequency of c is 4. After sorting the alphabets in the string aaajjbbbcccc based on frequency, the string becomes ccccaaabbbjj. 
+Example Input/Output 2: 
+Input:
+skillrack 
+Output: 
+kkllacirs  
+Example Input/Output 3: 
+Input: aaaBBBcCdddd
+Output: ddddBBBaaaCc
+----------------solution----------------
