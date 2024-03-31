c0ef79c20357997e054c5bddfd6a819714cdf385
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2024-03-31 17:31:44 +0530

    Create Substring Values of Length K.py

---
+++ Daily Test/Substring Values of Length K.py
@@ -0,0 +1,20 @@
+The program must accept a string S and an integer K as the input. The program must print all the substring values of length K in S as the output.
+Boundary Condition(s):
+1 <= K <= Length of S <= 100
+Input Format:
+The first line contains S and K separated by a space.
+Output Format:
+The first line contains all the substring values of length K in S separated by a space.
+Example Input/Output 1:
+Input:
+apple 3
+Output:
+app ppl ple
+Explanation:
+Here K = 3, all the substring values of length 3 in the string apple are app ppl ple.
+Hence the output is app ppl ple
+Example Input/Output 2:
+Input:
+skillrack 4
+Output:
+skil kill illr llra lrac rack
